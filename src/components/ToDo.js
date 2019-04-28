195112b736b7c563443a498de435f673cab8f9c5
David Salgado <david@digitalronin.com>
2019-04-28 12:06:40 +0100

    Add 'data-id' attr to list items
    I'm not sure if it's necessary, but that's the way
    it's done in this example: 
    
    https://github.com/SortableJS/react-sortablejs/blob/master/examples/index.jsx

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -2,7 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ListGroup from 'react-bootstrap/ListGroup';
 
-const ToDo = ({ title }) => <ListGroup.Item>{title}</ListGroup.Item>;
+const ToDo = ({ title }) => (
+  <ListGroup.Item data-id={title}>{title}</ListGroup.Item>
+);
 
 ToDo.propTypes = {
   title: PropTypes.string.isRequired
