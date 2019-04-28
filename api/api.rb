380d3454d35019b3f9021d1de35ec798f173df3e
David Salgado <david@digitalronin.com>
2019-04-28 17:03:33 +0100

    Convert API to a modular sinatra app.
    This will make it easier to add CORS configuration

--- api/api.rb
+++ api/api.rb
@@ -1,45 +1,51 @@
 #!/usr/bin/env ruby
 
 require 'bundler/setup'
-require 'sinatra'
+require 'sinatra/base'
 require 'json'
 
-class TodoList
-  @@items = []
-  @@counter = 0
+class Api < Sinatra::Base
+  set :bind, '0.0.0.0'
 
-  def self.to_json
-    @@items.to_json
-  end
+  class TodoList
+    @@items = []
+    @@counter = 0
 
-  def self.add_item(item)
-    item["_id"] = @@counter
-    @@items << item
-    @@counter += 1
-  end
-
-  def self.reorder(ids)
-    list = ids.map do |id|
-      @@items.find {|item| item['_id'] == id}
+    def self.to_json
+      @@items.to_json
     end
-    @@items = list
+
+    def self.add_item(item)
+      item["_id"] = @@counter
+      @@items << item
+      @@counter += 1
+    end
+
+    def self.reorder(ids)
+      list = ids.map do |id|
+        @@items.find {|item| item['_id'] == id}
+      end
+      @@items = list
+    end
   end
-end
 
-get '/api' do
-  TodoList.to_json
-end
+  get '/api' do
+    TodoList.to_json
+  end
 
-post '/api' do
-  content_type :json
-  data = JSON.parse request.body.read
-  TodoList.add_item data["todo"]
-  201
-end
+  post '/api' do
+    content_type :json
+    data = JSON.parse request.body.read
+    TodoList.add_item data["todo"]
+    201
+  end
 
-put '/api/reorder' do
-  content_type :json
-  list = JSON.parse request.body.read
-  TodoList.reorder list
-  TodoList.to_json
+  put '/api/reorder' do
+    content_type :json
+    list = JSON.parse request.body.read
+    TodoList.reorder list
+    TodoList.to_json
+  end
+
+  run! if app_file == $0
 end
