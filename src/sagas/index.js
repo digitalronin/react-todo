74cf8e27f5c78c1ae0d5e0a99f651f534d45ac36
David Salgado <david@digitalronin.com>
2019-04-28 18:06:22 +0100

    Post new todo items to the API
    This version follows up with an unnecessary fetch.
    We can just use the response from the POST, which
    contains the full list. The next commit will take
    care of that.

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,5 +1,6 @@
 import { all, call, put, takeEvery, takeLatest } from 'redux-saga/effects';
 import {
+  ADD_TODO,
   LOAD_TODO_LIST,
   RENDER_TODO_LIST,
   HANDLE_AUTHENTICATION_CALLBACK,
@@ -28,6 +29,19 @@ export function* handleAuthenticationCallback() {
   yield takeLatest(HANDLE_AUTHENTICATION_CALLBACK, parseHash);
 }
 
+export function* addToDoItem() {
+  yield takeEvery(ADD_TODO, asyncAddToDoItem);
+}
+
+export function* asyncAddToDoItem(action) {
+  const response = yield call(fetch, API, {
+    method: 'POST',
+    body: JSON.stringify(action.toDoItem),
+    headers: { 'Content-Type': 'application/json' }
+  });
+  yield put({ type: LOAD_TODO_LIST });
+}
+
 export default function* rootSaga() {
-  yield all([loadToDoList(), handleAuthenticationCallback()]);
+  yield all([loadToDoList(), addToDoItem(), handleAuthenticationCallback()]);
 }
