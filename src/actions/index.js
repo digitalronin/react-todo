90e7eb0aff20269d2fb1f141208fd2bc06f69973
David Salgado <david@digitalronin.com>
2019-04-28 14:06:45 +0100

    Add reorder list action

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,7 @@ export const LOAD_TODO_LIST = 'LOAD_TODO_LIST';
 export const RENDER_TODO_LIST = 'RENDER_TODO_LIST';
 export const USER_PROFILE_LOADED = 'USER_PROFILE_LOADED';
 export const HANDLE_AUTHENTICATION_CALLBACK = 'HANDLE_AUTHENTICATION_CALLBACK';
+export const REORDER_LIST = 'REORDER_LIST';
 
 export function loadToDoList() {
   return {
@@ -23,5 +24,12 @@ export function addToDo(title) {
 export function handleAuthenticationCallback() {
   return {
     type: HANDLE_AUTHENTICATION_CALLBACK
+  };
+}
+
+export function reorderList(listIds) {
+  return {
+    type: REORDER_LIST,
+    listIds
   };
 }
